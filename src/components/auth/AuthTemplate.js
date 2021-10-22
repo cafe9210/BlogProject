c6fa0b6cff7f75d6ab8d764239e0a109869ccada
ryeongeunkim <ryeongeun.gim@gmail.com>
2021-10-22 10:17:34 +0900

    21.10.22(금) 스니펫 추가

---
+++ src/components/auth/AuthTemplate.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+// 회원가입/로그인 페이지의 레이아웃을 담당하는 컴포넌트
+
+const AuthTempleteBlock = styled.div``;
+
+const AuthTemplete = () => {
+    return (
+        <AuthTempleteBlock>
+
+        </AuthTempleteBlock>
+    );
+};
+
+export default AuthTemplete;
