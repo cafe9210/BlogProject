3d556a28603f2737521916772c6e5a6be6cc8a41
linger0310 <goodbye1202@kakao.com>
2021-10-22 14:24:05 +0900

    ADD : 2021-10-22/예준/회원관리 부분 API

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,17 @@
+import { all } from '@redux-saga/core/effects';
 import {combineReducers} from 'redux'
-import auth from './auth'
+import auth, {authSaga} from './auth'
+import loading from './loading';
 
+// 프로젝트 루트 리듀서
 const rootReducer = combineReducers({
     auth,
+    loading, // 프로젝트 루트 리듀서에 modules/loading.js 코드(loading 리덕스 모듈 리듀서)를 등록한다.
 });
+
+// 프로젝트 루트 사가
+export function* rootSaga() {
+    yield all([authSaga()]);
+}
 
 export default rootReducer;
